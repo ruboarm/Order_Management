dbf1b32cd1d49d4c4acfe6a7de50b699af8825da
ruboarm <94679525+ruboarm@users.noreply.github.com>
2022-09-16 12:08:48 +0400

    xUnit Test for API

---
+++ Order_Management_App/UnitTest_OM_xUnit/SupplierControllerTest.cs
@@ -0,0 +1,89 @@
+using API.Controllers;
+using API.Data;
+using API.Models;
+using System;
+using System.Linq;
+using UnitTest_Order_Management;
+using Xunit;
+
+namespace UnitTest_OM_xUnit
+{
+    public class SupplierControllerTest : IClassFixture<TestDatabaseFixture>
+    {
+        public SupplierControllerTest(TestDatabaseFixture fixture)
+            => Fixture = fixture;
+
+        public TestDatabaseFixture Fixture { get; }
+
+        [Fact]
+        public void GetTest()
+        {
+            using var context = Fixture.CreateContext();
+            var controller = new SuppliersController(context);
+
+            var suppliers = controller.Get().Result;
+
+            Assert.Equal(2, suppliers.Count);
+        }
+
+        [Fact]
+        public void GetByIdTest()
+        {
+            using var context = Fixture.CreateContext();
+            var controller = new SuppliersController(context);
+
+            var suppliers = controller.Get().Result;
+            var id = suppliers.FirstOrDefault(s => s.SupplierName == "Supplier2").Id;
+            var supplier = controller.Get(id).Result;
+
+            Assert.Equal("Supplier2", supplier.SupplierName);
+        }
+
+        [Fact]
+        public void PostTest()
+        {
+            using var context = Fixture.CreateContext();
+            var controller = new SuppliersController(context);
+
+            var supplier = new Supplier()
+            {
+                SupplierName = "Supplier3",
+                AddressLine1 = "489 Ayre str",
+                AddressLine2 = "apt 2",
+                City = "Vanadzor",
+                PostalCode = 489,
+                State = "Available",
+            };
+            var updatedSupplier = controller.Post(supplier).Result;
+
+            Assert.Equal("Vanadzor", updatedSupplier.City);
+        }
+
+        [Fact]
+        public void PutTest()
+        {
+            using var context = Fixture.CreateContext();
+            var controller = new SuppliersController(context);
+
+            var suppliers = controller.Get().Result;
+            var supplier = suppliers.FirstOrDefault(s => s.SupplierName == "Supplier1");
+            supplier.City = "Armavir";
+            var updatedSupplier = controller.Put(supplier.Id, supplier).Result;
+
+            Assert.Equal("Armavir", updatedSupplier.City);
+        }
+
+        [Fact]
+        public void DeleteTest()
+        {
+            using var context = Fixture.CreateContext();
+            var controller = new SuppliersController(context);
+
+            var suppliers = controller.Get().Result;
+            var supplier = suppliers.FirstOrDefault(s => s.SupplierName == "Supplier2");
+            var code = controller.Delete(supplier.Id).Result;
+
+            Assert.True(true, code.ToString());
+        }
+    }
+}
---
+++ Order_Management_App/UnitTest_OM_xUnit/TestDatabaseFixture.cs
@@ -0,0 +1,64 @@
+﻿using API.Models;
+using API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTest_Order_Management
+{
+    // Document: https://docs.microsoft.com/en-us/ef/core/testing/testing-with-the-database
+    public class TestDatabaseFixture
+    {
+        private const string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=OrderManagementAPIDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public TestDatabaseFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+
+                        context.AddRange(
+                            new Supplier
+                            {
+                                SupplierName = "Supplier1",
+                                AddressLine1 = "132 Ride str",
+                                AddressLine2 = "apt 3",
+                                City = "Yerevan",
+                                PostalCode = 111,
+                                State = "Available",
+                            },
+                            new Supplier
+                            {
+                                SupplierName = "Supplier2",
+                                AddressLine1 = "132 Ride str",
+                                AddressLine2 = "apt 3",
+                                City = "Yerevan",
+                                PostalCode = 111,
+                                State = "Available",
+                            });
+                        context.SaveChanges();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public ApplicationDbContext CreateContext()
+            => new ApplicationDbContext(
+                new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlServer(ConnectionString)
+                    .Options);
+    }
+}
